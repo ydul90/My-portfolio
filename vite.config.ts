4745e57c71083274099ec9e36148df885f604f10
Ydul_Conag <ludybongconag0@gmail.com>
2026-06-23 23:13:44 +0800

    Standardize build and fix 404 errors

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
